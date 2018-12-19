097fecb9be177fcbbbbeda72040ef88f0ac03a0c
bartwr <mail@bartroorda.nl>
2018-12-19 11:49:26 +0100

    Added example Meetup API requests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+var meetup = require('meetup-api')();
+
 app.set("port", process.env.PORT || 3001);
 
 // Express only serves static assets in production
@@ -8,7 +10,30 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("/api/hi", (req, res) => {
+app.get("/api/findLocations", (req, res) => {
+
+  // meetup.findLocations({
+  //   query: 'Córdoba'
+  // }, function (err, results) {
+  //     console.log(results);
+  // });
+
+  // const param = req.query.q;
+  res.json({
+    'test': 'test'
+  });
+});
+
+app.get("/api/getCities", (req, res) => {
+
+  // meetup.getCities({
+  //   lat: -34.603722,
+  //   lon: -58.381592,
+  //   country: 'AR'
+  // }, function (err, results) {
+  //     console.log(results);
+  // });
+
   // const param = req.query.q;
   res.json({
     'test': 'test'
