b406ebb0b243769cd2e8b96eda2be6a9217c45b6
bartwr <mail@bartroorda.nl>
2018-12-19 11:49:32 +0100

    Added basic HTML

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  getEvents() {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <img src="https://secure.meetup.com/s/img/0/logo/svg/logo--script.svg" width="200" alt="Meetup image" class="center" />
+
+        <div id="results"> 
+          <section>
+          </section>
+        </div> 
+
       </div>
     );
   }
