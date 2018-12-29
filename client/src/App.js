c5c762a9652f53612f36917b96bea3df849886fa
bartwr <mail@bartroorda.nl>
2018-12-29 22:55:29 +0100

    Added basic intro text

--- client/src/App.js
+++ client/src/App.js
@@ -244,8 +244,27 @@ class App extends Component {
 
     return (
       <div className="App">
+        <div class="App-intro">
 
-      	<img src="https://secure.meetup.com/s/img/0/logo/svg/logo--script.svg" width="200" alt="Meetup logo" />
+          <h1>The Hague - Tech</h1>
+
+          <p>
+            We are happy to see that <a href="https://denhaag.com/en" target="_blank">The Hague</a> is listed as one of the top growing tech cities in the world.
+          </p>
+
+          <p>
+            We are members of The Hague Tech, a tech hub of +- 1 year old. We were curious if The Hague Tech has played a big role in this growth, so we did some research.
+          </p>
+
+          <p>
+            A interactive overview of what happened in The Hague meetup scene last years.
+          </p>
+
+        </div>
+
+        <p>
+      	  <img src="https://secure.meetup.com/s/img/0/logo/svg/logo--script.svg" width="200" alt="Meetup logo" />
+        </p>
 
       	<section className="Stats-the-hague">
 
