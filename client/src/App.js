df2e959a89543740a1cb3af4cfa974d5991e1504
bartwr <mail@bartroorda.nl>
2018-12-30 23:51:00 +0100

    Added footer

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,10 @@ class AppTab extends Component {
           <StatsList year={this.props.year} />
         </div>
 
+        <footer className="Footer link-style-10">
+          This project was initiated by <a href="https://www.linkedin.com/in/charlesulin/" target="_blank" rel="noopener noreferrer">Charles Weiler-Ulin</a>, <a href="https://github.com/Deucemon" target="_blank" rel="noopener noreferrer">Deucemon</a> & <a href="https://www.bartroorda.nl" target="_blank" rel="noopener noreferrer">bartwr</a>. You're welcome to contribute to this project: Share your ideas via <a href="mailto:mail@bartroorda.nl?subject=Hello from the-hague-meetup-stats&body=I like to">mail@bartroorda.nl</a> or start working on improving <a href="https://github.com/Deucemon/Meetup-THT-events" target="_blank" rel="noopener noreferrer">the code</a>.
+        </footer>
+
       </div>
     )
   }
