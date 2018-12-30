58ece560073ef1274ca2a2d39ff32a64d7b11f15
bartwr <mail@bartroorda.nl>
2018-12-30 15:54:16 +0100

    Updated intro text

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,24 @@ class App extends Component {
 
         <TextBlock title="The Hague - Tech">
 
-          <p>
+          <p class="link-style-10">
             We are happy to see that <a href="https://denhaag.com/en" target="_blank">The Hague</a> is <a href="https://2018.stateofeuropeantech.com/chapter/tech-hubs/article/growing-communities/#chart-25" target="_blank">listed</a> as one of the top growing tech cities in the world.
           </p>
 
-          <p>
-            We are members of The Hague Tech, a tech hub of +- 1 year old. We were curious if The Hague Tech has played a big role in this growth, so we did some research.
+          <p class="link-style-10">
+            We were curious: How much of the tech community growth was caused by <a href="https://www.thehaguetech.com/">The Hague Tech</a>, a new tech hub that exists for +- 1 year now.
           </p>
 
-          <p>
-            A interactive overview of what happened in The Hague meetup scene last years.
+          <p class="link-style-10">
+            On this page you find some nice stats, based on the <img src="/logo/meetup.svg" height="30" alt="Meetup" style={{position: 'relative', top: '7px'}} /> API. All is <a href="https://github.com/Deucemon/Meetup-THT-events" target="_blank">open source</a>, feel free to contribute. Enjoy!
           </p>
 
         </TextBlock>
 
         <div className="flex" style={{margin: '40px 0'}}>
-          <ChartBlock title="Groups" primaryStat="200" secondaryStat="of which 10 at The Hague Tech" backgroundColor="#b35da0" />
-          <ChartBlock title="RSVP's" primaryStat="200" secondaryStat="of which 10 at The Hague Tech" backgroundColor="#f28a17" />
-          <ChartBlock title="Events" primaryStat="200" secondaryStat="of which 10 at The Hague Tech" backgroundColor="#f0c214" />
+          <ChartBlock title="Groups" primaryStat="200" secondaryStat="10 at The Hague Tech" backgroundColor="#b35da0" />
+          <ChartBlock title="RSVP's" primaryStat="200" secondaryStat="10 at The Hague Tech" backgroundColor="#f28a17" />
+          <ChartBlock title="Events" primaryStat="200" secondaryStat="10 at The Hague Tech" backgroundColor="#f0c214" />
         </div>
 
         <RsvpsThroughTime />
